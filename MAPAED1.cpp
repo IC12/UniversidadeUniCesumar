d52a181d4d99c06a2aa03614e7568d6ffb7a0c81
IC12 <iaracamargos@hotmail.com>
2024-03-29 20:31:19 -0300

    2º Programa upgrade

--- MAPAED1.cpp
+++ MAPAED1.cpp
@@ -1,6 +1,3 @@
-// Olá professores, fiz esse MAPA com toda dedicação e sem colas. Apenas olhei exemplos e tirei algumas dúvidas para tentar realizar meu código de acordo com o proposto.
-// Fiquei quebrando cabeça há algum tempo mas infelizmente não consegui realiza-lo da maneira proposta ;-( Aguardo feedback.
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
@@ -8,273 +5,134 @@
 
 #define tamanho 5
 
-struct fila{
-	int dados [tamanho];
+typedef struct info_aluno {
 	int ra;
 	char arq[100];
 	char ext[4];
 	int matr;
-	int ini;
+}info_aluno;
+
+typedef struct tipo_fila {
+    info_aluno dados [tamanho];
+    int ini;
 	int fim;
-};
+}tipo_fila;
 
-struct fila fila;
-int op;
+tipo_fila fila_alunos;
 
-void enfileira(); 
-void desenfileira(); 
-void fila_mostrar();
-void menu();
+void iniciaFila(tipo_fila *f) {
+    int i;
+    
+    f->ini = 0;
+    f->fim = 0;
+    
+    for(i = 0; i < tamanho; i++) {
+        f-> dados[i].ra = -1;
+        strcpy(f-> dados[i].arq, "");
+        strcpy(f-> dados[i].ext, "");
+        f-> dados[i].matr = -1;
+    }
+}
 
-void enfileira (){
-	
-	int ra;
-	char arq[100];
-	char ext[4];
-	int matr;
-	
-	if (fila.fim == tamanho){
-		printf("Fila cheia!\n");
-		system ("pause");
-	}
-	else
-	{
-		printf("\nRA do aluno: ");
-		scanf("%d", &ra);
-		fflush(stdin);
-		printf("\nNome do arquivo: ");
-		scanf("%s", &arq);
-		fflush(stdin);
-		printf("\nExtensão do arquivo: ");
-		scanf("%s", &ext);
-		fflush(stdin);
-		printf("\nMatrícula do professor: ");
-		scanf("%d", &matr);
-		fflush(stdin);
-		fila.fim++;
-	}
+void enfileira(info_aluno elemento, tipo_fila *f) {
+    if(f-> fim == tamanho) {
+        printf("Fila cheia.\n");
+        system("pause");
+    } else {
+        f-> dados[f->fim] = elemento;
+        f-> fim++;
+    }
 }
-void desenfileira() {
-	
-	int ra;
-	char arq[100];
-	char ext[4];
-	int matr;
-	
-	if (fila.ini == fila.fim) {
-		printf("\nFila vazia!\n\n");
-		system("pause");
-	}
-	else {
-		int i;
-		for (i = 0; i < tamanho; i++) {
-			fila.dados[i]=fila.dados[i+1];
-		fila.dados[fila.fim]=0;
-		printf("RA do aluno: %d\n", &ra);
-		printf("Nome do arquivo: %s\n", &arq);
-		printf("Extensão do arquivo: %s\n", &ext);
-		printf("Matrícula do professor: %d\n\n", &matr);
-		
-		fila.fim--;
-		}
-	}
+
+info_aluno desenfileira(tipo_fila *f) {
+    info_aluno elemento;
+    int i;
+    
+    if(f-> fim == f-> ini) {
+        printf("Fila vazia. \n");
+        system("pause");
+    } else {
+        elemento = f-> dados[f-> ini];
+        for(i = 0; i < tamanho; i++) {
+            f-> dados[i] = f-> dados[i+1];
+            f-> fim--;
+        }
+    }
+    return elemento;
 }
-void esvaziar(){
-	fila.ini = 0;
-	fila.fim = 0;
-	
-	void fila_mostrar();
+
+void imprimeFila(tipo_fila *f) {
+    int i;
+    printf("\n\nLista de alunos:\n\n");
+    for(i = 0; i < f-> fim; i++) {
+        printf("%d\n", f-> dados[i].ra);
+        printf("%s\n", f-> dados[i].arq);
+        printf("%s\n", f-> dados[i].ext);
+        printf("%d\n\n", f-> dados[i].matr);
+    }
 }
-void fila_mostrar(){
-	
-	int ra;
-	char arq[100];
-	char ext[4];
-	int matr;
-	int i;
-	for (i = 0; i < tamanho; i++) {
-		printf("RA do aluno: %d\n", &ra);
-		printf("Nome do arquivo: %s\n", &arq);
-		printf("Extensão do arquivo: %s\n", &ext);
-		printf("Matrícula do professor: %d\n\n", &matr);
-		
-	}
-}
-void menu() {  
-	printf("1 - Inserir um elemento na fila;\n");          
-	printf("2 - Remover um elemento da fila;\n");
+
+void menu() {
+    imprimeFila(&fila_alunos);
+    
+    printf("\n\nOpÃ§Ãµes:\n");
+    printf("1 - Inserir um aluno na fila;\n");
+    printf("2 - Remover um aluno da fila;\n");
 	printf("3 - Esvaziar a fila;\n");                          
 	printf("0 - Encerrar o programa.\n\n");        
-	}
-	
-int main(){
-	
-setlocale(LC_ALL, "Portuguese");
-
-	op = 0;
-	fila.fim = 0;
-	fila.ini = 0;
-	   
-	system("cls");
-	fila_mostrar();       
-	menu(); 
-	
-	do{
-	scanf("%d", &op);
-	fflush(stdin);
-		switch (op) {      
-		case 1:        
-		enfileira ();      
-		break;
-		case 2:        
-		desenfileira();      
-		break;
-		case 3:
-		esvaziar();
-		break;
-		case 0:
-		printf("Encerrado!\n");
-			break;
-		
-		default:
-			printf("Erro: opção inválida!\n\n");  
-		}        
-	}while(op != 0);  
-	 
-	return 0; 
 }
 
-// Tentei fazer de várias formas, mas de acordo com a proposta eu não conseguir. Segue abaixo o outro código que eu criei.
-
-//#include <stdio.h>
-//#include <stdlib.h>
-//#include <locale.h>
-//#include <string.h>
-
-//#define tamanho 5
-
-//struct fila{
-//	int dados [tamanho];
-//	int ra;
-//	char arq[100];
-//	char ext[4];
-//	int matr;
-//	int ini;
-//	int fim;
-//};
-
-//fila fila;
-//int op;
-
-//void enfileira(); 
-//void desenfileira(); 
-//void fila_mostrar();
-//void menu();
-
-//void enfileira (){
-	
-//	if (fila.fim == tamanho){
-//		printf("Fila cheia!\n");
-//		system ("pause");
-//	}
-//	else
-//	{
-//		printf("\nRA do aluno: ");
-//		scanf("%d", fila.ra);
-//		fflush(stdin);
-//		printf("\nNome do arquivo: ");
-//		scanf("%s", fila.arq);
-//		fflush(stdin);
-//		printf("\nExtensão do arquivo: ");
-//		scanf("%s", fila.ext);
-//		fflush(stdin);
-//		printf("\nMatrícula do professor: ");
-//		scanf("%d", fila.matr);
-//		fflush(stdin);
-//		fila.fim++;
-//	}
-//}
-//void desenfileira() {
-//	
-//	if (fila.ini == fila.fim) {
-//		printf("\nFila vazia!\n\n");
-//		system("pause");
-//	}
-//	else {
-//		int i;
-//		for (i = 0; i < tamanho; i++) {
-//			fila.dados[i]=fila.dados[i+1];
-//		fila.dados[fila.fim]=0;
-//		printf("RA do aluno: %d\n", fila.ra);
-//		printf("Nome do arquivo: %s\n", fila.arq);
-//		printf("Extensão do arquivo: %s\n", fila.ext);
-//		printf("Matrícula do professor: %d\n\n", fila.matr);
-		
-//		fila.fim--;
-//		}
-//	}
-//}
-//void esvaziar(){
-//	fila.ini = 0;
-//	fila.fim = 0;
-	
-//	void fila_mostrar();
-//}
-//void fila_mostrar(){
-	
-//	int i;
-//	for (i = 0; i < tamanho; i++) {
-//		printf("RA do aluno: %d\n", fila.ra);
-//		printf("Nome do arquivo: %s\n", fila.arq);
-//		printf("Extensão do arquivo: %s\n", fila.ext);
-//		printf("Matrícula do professor: %d\n\n", fila.matr);
-		
-//	}
-//}
-//void menu() {  
-//	printf("1 - Inserir um elemento na fila;\n");          
-//	printf("2 - Remover um elemento da fila;\n");
-//	printf("3 - Esvaziar a fila;\n");                          
-//	printf("0 - Encerrar o programa.\n\n");        
-//	}
-	
-//int main(){
-	
-//setlocale(LC_ALL, "Portuguese");
-
-//	op = 0;
-//	fila.fim = 0;
-//	fila.ini = 0;
-	   
-//	system("cls");
-//	fila_mostrar();       
-//	menu(); 
-	
-//	do{
-//	scanf("%d", &op);
-//	fflush(stdin);
-//		switch (op) {      
-//		case 1:        
-//		enfileira ();      
-//		break;
-//		case 2:        
-//		desenfileira();      
-//		break;
-//		case 3:
-//		esvaziar();
-//		break;
-//		case 0:
-//		printf("Encerrado!\n");
-//			break;
-		
-//		default:
-//			printf("Erro: opção inválida!\n\n");  
-//		}        
-//	}while(op != 0);  
-	 
-//	return 0; 
-//}
-
-
-
-
+int main(int argc, char *argv[]) {
+    setlocale(LC_ALL, "Portuguese");
+    
+    int op;
+    info_aluno aluno;
+    
+    iniciaFila(&fila_alunos);
+    
+    do {
+        menu();
+        scanf("%d", &op);
+        fflush(stdin);
+        
+        switch(op) {
+            case 1:
+                if(fila_alunos.fim == tamanho) {
+                    printf("Fila cheia.\n");
+                    system("pause");
+                } else {
+                    printf("Informe o RA do aluno:\n");
+                    scanf("%d", &aluno.ra);
+                    fflush(stdin);
+                    printf("\nNome do arquivo: ");
+                    scanf("%99[^\n]s", aluno.arq);
+                    fflush(stdin);
+                    printf("\nExtensÃ£o do arquivo: ");
+                    scanf("%3[^\n]s", aluno.ext);
+                    fflush(stdin);
+                    printf("\nMatrÃ­cula do professor: ");
+                    scanf("%d", &aluno.matr);
+                    fflush(stdin);
+                    enfileira(aluno, &fila_alunos);
+                }
+                break;
+            case 2:
+                if(fila_alunos.fim != 0) {
+                    aluno = desenfileira(&fila_alunos);
+                    printf("Desenfileirando: %d - %s - %s - %d.\n", aluno.ra, aluno.arq, aluno.ext, aluno.matr);
+                } else
+                    printf("Fila vazia.\n");
+                system("pause");
+                break;
+            case 3:
+                iniciaFila(&fila_alunos);
+                printf("A fila foi esvaziada!\n");
+                system("pause");
+                break;
+            default:
+                if(op)
+                    printf("OpÃ§Ã£o invÃ¡lida!\n");
+                break;
+        }
+    } while(op != 0);
+}
--- README.md
+++ README.md
@@ -3,7 +3,7 @@ __Curso Análise e Desenvolvimento de Sistemas__
 - Trabalho (MAPA): 1º programa
       entendendo conceitos básicos de linguagem C;
 - Trabalho (MAPA): 2º programa
-      realizado em junho de 2021. Tive muita dificuldade em fazer. Que continue os desafios para aprendizado ;-);
+      realizado em junho de 2021. Tive dificuldade em fazer.** (\**) Em 2024 fiz um upgrade e felizmente avaliei meu progresso;
 - Atividades em C/C++;
 - Introdução ao JAVA exercícios;
 - Trabalho (MAPA): programa em JAVA
